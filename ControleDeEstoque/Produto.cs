79961cb8357f4ef400313d9138733f441071d3ff
saboop99 <rafa4021@hotmail.com>
2021-09-22 17:51:35 -0300

    Utilização do Autoproperties no projeto

--- ControleDeEstoque/Produto.cs
+++ ControleDeEstoque/Produto.cs
@@ -8,10 +8,13 @@ namespace ControleDeEstoque
 {
     public class Produto
     {
-        //declaração dos dados do produto, agora em private para o encapsulamento
+        /*declaração dos dados do produto
+          unico private é o nome, por não estar setado ("regra de negócios")
+          Autoproperties usado no Preço e na Quantidade, sendo inputados automaticamente com o get e set, bloquando que o usuário altere os valores
+         */
         private string _nome;
-        private double _preço;
-        private int _quantidade;
+        public double Preço { get; private set; }
+        public int Quantidade { get; private set; }
 
         //construtor padrão
         public Produto()
@@ -22,46 +25,43 @@ namespace ControleDeEstoque
         public Produto (string nome, double preço, int quantidade)
         {
             _nome = nome;
-            _preço = preço;
-            _quantidade = quantidade;
+            Preço = preço;
+            Quantidade = quantidade;
                 
         }
 
-       
-        
-        // utilização do Get (encapsulamento), para obter o nome
-        public string GetNome()
+        // Execução do Properties, sendo "Nome" como se fosse um atributo (desta vez sendo executado apenas no nome)
+        public string Nome
         {
-            return _nome;
-        }
-        
-        // utilização do Set(encapsulamento), para definir o nome
-        public void SetNome(string nome)
-        {
-            _nome = nome;
+            //método GET para obter o nome
+            get { return _nome; }
+            // método SET para definir o nome
+            set
+            {
+                //condicional If para definir o nome (diferente de nulo e maior que 1)
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
         }
 
-        // utilização do Get (encapsulamento), para obter a quantidade
-        public int GetQuantidade()
-        {
-            return _quantidade;
-        } 
         //operação matemática para definir o valor total em estoque
         public double ValorTotalEmEstoque()
         {
-            return _preço * _quantidade;
+            return Preço * Quantidade;
         }
 
         //operação para definir a adição de produtos ao estoque
         public void AdicionarProduto(int quantidade)
         {
-            _quantidade += quantidade;
+            Quantidade += quantidade;
         }
         
         // operação para definir a subtração de produtos ao estoque
         public void RemoverProduto(int quantidade)
         {
-            _quantidade = _quantidade - quantidade;
+            Quantidade = Quantidade - quantidade;
         }
         
         //fazer o override para mostrar os dados na tela com as modificações dos valores digitados pelo usuário, dando a ele os dados atualizados
@@ -69,9 +69,9 @@ namespace ControleDeEstoque
         {
             return _nome 
                 + ", R$" 
-                + _preço.ToString("F2", CultureInfo.InvariantCulture) 
+                + Preço.ToString("F2", CultureInfo.InvariantCulture) 
                 + ", " 
-                + _quantidade 
+                + Quantidade
                 + " unidades, Total: R$ " 
                 + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
