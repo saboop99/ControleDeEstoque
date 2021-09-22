2380a3f43a7edcaf6270a370bf8919e73689e529
saboop99 <rafa4021@hotmail.com>
2021-09-22 16:49:04 -0300

    Atualização encapsulamento

--- ControleDeEstoque/Produto.cs
+++ ControleDeEstoque/Produto.cs
@@ -8,49 +8,73 @@ namespace ControleDeEstoque
 {
     public class Produto
     {
-        //declaração dos dados do produto
-        public string Nome;
-        public double Preço;
-        public int Quantidade;
+        //declaração dos dados do produto, agora em private para o encapsulamento
+        private string _nome;
+        private double _preço;
+        private int _quantidade;
 
-        // definição do método construtor, para ter uma memória reserva
+        //construtor padrão
+        public Produto()
+        {
+
+        }
+        // definição do método construtor personalizado, para ter uma memória reserva
         public Produto (string nome, double preço, int quantidade)
         {
-            Nome = nome;
-            Preço = preço;
-            Quantidade = quantidade;
+            _nome = nome;
+            _preço = preço;
+            _quantidade = quantidade;
                 
         }
 
+       
+        
+        // utilização do Get (encapsulamento), para obter o nome
+        public string GetNome()
+        {
+            return _nome;
+        }
+        
+        // utilização do Set(encapsulamento), para definir o nome
+        public void SetNome(string nome)
+        {
+            _nome = nome;
+        }
+
+        // utilização do Get (encapsulamento), para obter a quantidade
+        public int GetQuantidade()
+        {
+            return _quantidade;
+        } 
         //operação matemática para definir o valor total em estoque
         public double ValorTotalEmEstoque()
         {
-            return Preço * Quantidade;
+            return _preço * _quantidade;
         }
 
         //operação para definir a adição de produtos ao estoque
         public void AdicionarProduto(int quantidade)
         {
-            Quantidade += quantidade;
+            _quantidade += quantidade;
         }
-
+        
         // operação para definir a subtração de produtos ao estoque
         public void RemoverProduto(int quantidade)
         {
-            Quantidade = Quantidade - quantidade;
+            _quantidade = _quantidade - quantidade;
         }
-
+        
         //fazer o override para mostrar os dados na tela com as modificações dos valores digitados pelo usuário, dando a ele os dados atualizados
         public override string ToString()
         {
-            return Nome 
+            return _nome 
                 + ", R$" 
-                + Preço.ToString("F2", CultureInfo.InvariantCulture) 
+                + _preço.ToString("F2", CultureInfo.InvariantCulture) 
                 + ", " 
-                + Quantidade 
-                + "unidades, Total: R$ " 
+                + _quantidade 
+                + " unidades, Total: R$ " 
                 + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
     }
-    
+
 }
